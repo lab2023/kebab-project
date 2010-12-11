1ff09759b398e3c23facf530977b70bda97461d0
Tayfun Öziş Erikan <tayfun.ozis.erikan@lab2023.com>
2010-12-11 10:06:36 +0200

    New  application folder base

---
+++ application/Bootstrap.php
@@ -0,0 +1,75 @@
+<?php if ( ! defined('BASE_PATH')) exit('No direct script access allowed');
+/**
+ * Kebab Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the  Dual Licensing Model that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.kebab-project.com/licensing
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@lab2023.com so we can send you a copy immediately.
+ *
+ * @category   KEBAB
+ * @package    Core
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/licensing
+ * @version    1.5.0
+ */
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    private $_config;
+    private $_logger;
+
+    /*
+     * Config Initialization
+     */
+    protected function _initConfig()
+	{
+        $config = new Zend_Config($this->getOptions(), true);
+        Zend_Registry::set('config', $this->_config = $config);
+    }
+    
+    /*
+     * Logger Initialization
+     */
+    protected function _initLogging()
+	{
+	    $logger = new Zend_Log();
+
+	    //Empty Writer
+        $logger->addWriter(
+            new Zend_Log_Writer_Null()
+        );
+        
+        if($this->_config->kernel->logger->use) {
+            //Stream Writer
+            if($this->_config->kernel->logger->stream->use) {
+                $logger->addWriter(
+                    new Zend_Log_Writer_Stream(
+                        CORE_PATH . '/' .
+                        $this->_config->kernel->logger->folder. '/' .
+                        $this->_config->kernel->logger->file
+                    )
+                );
+            }
+            //Firebug Writer
+            if($this->_config->kernel->logger->firebug->use) {
+                $logger->addWriter(
+                    new Zend_Log_Writer_Firebug()
+                );
+            }
+        }
+
+        Zend_Registry::set('logger', $this->_logger = $logger);
+
+        // Info Log
+        $this->_logger->log(
+            'Logging initialized...',
+            Zend_Log::INFO
+        );
+	}
+}
---
+++ application/controllers/ErrorController.php
@@ -0,0 +1,81 @@
+<?php if ( ! defined('BASE_PATH')) exit('No direct script access allowed');
+/**
+ * Kebab Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the  Dual Licensing Model that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.kebab-project.com/licensing
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@lab2023.com so we can send you a copy immediately.
+ *
+ * @category   KEBAB
+ * @package    Core
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/licensing
+ * @version    1.5.0
+ */
+
+class ErrorController
+    extends Kebab_Controller_Action
+{
+
+    public function errorAction()
+    {
+        $errors = $this->_getParam('error_handler');
+        
+        if (!$errors) {
+            $this->view->message = 'You have reached the error page';
+            return;
+        }
+        
+        switch ($errors->type) {
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+        
+                // 404 error -- controller or action not found
+                $this->getResponse()->setHttpResponseCode(404);
+                $this->view->message = 'Page not found';
+                break;
+            default:
+                // application error
+                $this->getResponse()->setHttpResponseCode(500);
+                $this->view->message = 'Application error';
+                break;
+        }
+        
+        // Log exception, if logger available
+        if ($log = $this->getLog()) {
+            $log->crit($this->view->message, $errors->exception);
+        }
+        
+        // conditionally display exceptions
+        if ($this->getInvokeArg('displayExceptions') == true) {
+            $this->view->exception = $errors->exception;
+        }
+        
+        $this->view->request   = $errors->request;
+
+        Zend_Registry::get('logger')->log(
+            $errors,
+            Zend_Log::ERR
+        );
+    }
+
+    public function getLog()
+    {
+        $bootstrap = $this->getInvokeArg('bootstrap');
+        if (!$bootstrap->hasResource('Log')) {
+            return false;
+        }
+        $log = $bootstrap->getResource('Log');
+        return $log;
+    }
+
+
+}
+
