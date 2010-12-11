a82e29001f8f41804bff76e43d1470e0ca199ad6
Tayfun Öziş Erikan <tayfun.ozis.erikan@lab2023.com>
2010-12-11 10:01:33 +0200

    Change "app" folder name to "application"

--- core/libraries/Kebab/Test.php
+++
@@ -1,9 +0,0 @@
-<?php if ( ! defined('BASE_PATH')) exit('No direct script access allowed');
-
-class Kebab_Test
-{
-    public static function testMe()
-    {
-        return "Core Libs Test OK!";
-    }
-}
