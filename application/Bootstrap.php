07465e4c576227030d54ccd111d055cd4295ae1e
Tayfun Öziş Erikan <tayfun.ozis.erikan@lab2023.com>
2010-12-12 16:20:24 +0200

    Doctrine Initialized

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -32,7 +32,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $config = new Zend_Config($this->getOptions(), true);
         Zend_Registry::set('config', $this->_config = $config);
     }
-    
+
     /*
      * Logger Initialization
      */
@@ -72,4 +72,26 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
             Zend_Log::INFO
         );
 	}
+
+    public function _initDoctrine()
+    {
+        $this->getApplication()->getAutoloader()
+                               ->pushAutoloader(array('Doctrine', 'autoload'));
+        spl_autoload_register(array('Doctrine', 'modelsAutoload'));
+        
+        $manager = Doctrine_Manager::getInstance();
+        $manager->setAttribute(Doctrine::ATTR_AUTO_ACCESSOR_OVERRIDE, true);
+        $manager->setAttribute(
+            Doctrine::ATTR_MODEL_LOADING,
+            Doctrine::MODEL_LOADING_CONSERVATIVE
+        );
+        $manager->setAttribute(Doctrine::ATTR_AUTOLOAD_TABLE_CLASSES, true);
+
+        Doctrine::loadModel($this->_config->database->doctrine->models_path);
+
+        $conn = Doctrine_Manager::connection($this->_config->database->doctrine->dsn, 'doctrine');
+        $conn->setAttribute(Doctrine::ATTR_USE_NATIVE_ENUM, true);
+        
+        return $conn;
+    }
 }
