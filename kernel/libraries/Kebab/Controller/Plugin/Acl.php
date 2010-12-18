b9a5e3f1236b98f1261cdd6355cac98daae787ff
ooo <ooo@ooo-Aspire-5600>
2010-12-18 16:27:25 +0200

    Move ACL Rules to Kebab_Acl object

--- kernel/libraries/Kebab/Controller/Plugin/Acl.php
+++ kernel/libraries/Kebab/Controller/Plugin/Acl.php
@@ -4,16 +4,19 @@ class Kebab_Controller_Plugin_Acl extends Zend_Controller_Plugin_Abstract
 {
 
     private $_acl;
+    private $_roles = array();
 
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
         $auth = Zend_Auth::getInstance();
         if ($auth->hasIdentity()) {
             $identity = $auth->getIdentity();
-            $role = strtolower($identity->role);
+            $this->_roles = $identity->roles;
+            $this->_acl = $identity->acl;
         } else {
             //KBBTODO Don't like it' Hardcoding is bad!
-            $role = 'guest';
+            $this->_roles = array('guest');
+            $this->_acl = new Kebab_Acl();
         }
 
         // Create mvcResource
@@ -22,37 +25,17 @@ class Kebab_Controller_Plugin_Acl extends Zend_Controller_Plugin_Abstract
         $mvcResource = $module . '_' . $controller;
         $action = $request->getActionName();
 
-        self::_setAcl();
-        
-        if (!$this->_acl->isAllowed($role, $mvcResource, $action)) {
+        //Check Rules
+        $isAllowed = FALSE;
+        while (!$isAllowed && list(, $role) = each($this->_roles)) {
+            $isAllowed = $this->_acl->isAllowed($role, $mvcResource, $action);
+        }
+
+        if (!$isAllowed) {
             $request->setModuleName('default');
             $request->setControllerName('auth');
             $request->setActionName('login');
         }
-    }
-
-    private function _setAcl()
-    {
-        $acl = new Zend_Acl();
-
-        //Add Role
-        $acl->addRole(new Zend_Acl_Role('guest'));
-        $acl->addRole(new Zend_Acl_Role('member'), 'guest');
-        $acl->addRole(new Zend_Acl_Role('admin'), 'member');
-        $acl->addRole(new Zend_Acl_Role('owner'), 'admin');
-
-        //Add Resource
-        $acl->add(new Zend_Acl_Resource('Default_Index'));
-        $acl->add(new Zend_Acl_Resource('Default_Auth'));
-        $acl->add(new Zend_Acl_Resource('Default_Error'));
-        $acl->add(new Zend_Acl_Resource('Default_Main'));
-
-        // Rules
-        $acl->allow('guest', 'Default_Auth', array('login', 'logout'));
-        $acl->allow('guest', 'Default_Error');
-        $acl->allow('member', 'Default_Main');
-
-        $this->_acl = $acl;
     }
 
 }
