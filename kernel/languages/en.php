dddb90a2f8a6df0976e1e9cbf3c2c9ea8516334f
Onur Özgür ÖZKAN <ooo@ooo-Aspire-5600.(none)>
2010-12-18 16:53:50 +0200

    Legacy folders remove and sync

--- kernel/languages/en.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return array(
-    'KERNEL_ONE' => 'Kernel One',
-    'KERNEL_TWO' => 'Kernel Two',
-    'KERNEL_THREE' => 'Kernel Three'
-);
--- kernel/languages/tr.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-    'KERNEL_ONE' => 'Kernel Bir',
-    'KERNEL_TWO' => 'Kernel İki',
-    'KERNEL_THREE' => 'Kernel Üç'
-);
-
