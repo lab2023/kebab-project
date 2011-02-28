8505678293e4503aae42ba178cabad0dc415e7a6
onur.ozgur.ozkan <onur.ozgur.ozkan@lab2023.com>
2011-02-28 17:04:54 +0200

    Add Kebab_Rest_Controller. Aim is set layout disable and noviewrenderer on init() methods

---
+++ library/Kebab/Rest/Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Kebab Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the  Dual Licensing Model that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.kebab-project.com/licensing
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@lab2023.com so we can send you a copy immediately.
+ *
+ * @category   Kebab (kebab-reloaded)
+ * @package    PACKAGE
+ * @subpackage SUB_PACKAGE
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/licensing
+ * @version    1.5.0
+ */
+
+/**
+ *
+ * @category    Kebab
+ * @package     Kebab_Rest
+ * @see         Zend_Rest_Route
+ * @copyright   Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license     http://www.kebab-project.com/licensing
+ */
+abstract class Kebab_Rest_Controller extends Zend_Rest_Controller
+{
+    /**
+     * Set layout disable and no view renderer
+     */
+    public function init()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender();
+    }
+}
