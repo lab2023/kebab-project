ca24427795bf36f38f09867e3c1792a54a356a85
onur.ozgur.ozkan <onur.ozgur.ozkan@lab2023.com>
2011-03-06 22:30:32 +0200

    [#10783831] add a filter controller helper

---
+++ library/Kebab/Controller/Helper/Filter.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Kebab Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the  Dual Licensing Model that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.kebab-project.com/licensing
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@lab2023.com so we can send you a copy immediately.
+ *
+ * @category   Kebab (kebab-reloaded)
+ * @package    PACKAGE
+ * @subpackage SUB_PACKAGE
+ * @author     lab2023 Dev Team
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/licensing
+ * @version    1.5.0
+ */
+
+
+/**
+ * System_Controller_Helper_Filter
+ *
+ * @category   Kebab (kebab-reloaded)
+ * @package    Controller
+ * @subpackage Helper
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/licensing
+ * @since      1.5.x (kebab-reloaded)
+ * @version    1.5.0
+ */
+class System_Controller_Helper_Filter extends Zend_Controller_Action_Helper_Abstract
+{
+    //put your code here
+}
+
