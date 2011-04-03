322de49e2b570394d2a728a30f80897004fe482e
onur.ozgur.ozkan <onur.ozgur.ozkan@lab2023.com>
2011-04-03 16:02:48 +0300

    Add Profile controller to Zend Rest Routing

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -217,7 +217,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
             array(
                  'access'=> array('user-role'),
                  'authentication'=> array('session'),
-                 'user'=> array('user')
+                 'user'=> array('user', 'profile')
             )
         );
         $front->getRouter()->addRoute('rest', $restRoute);
