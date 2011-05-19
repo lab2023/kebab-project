0872465a35ca707b4df93effc9cac0d08ef0ae3a
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-05-19 09:39:31 +0300

    Add Kebab Module

---
+++ application/modules/kebab/languages/tr.php
@@ -0,0 +1,42 @@
+<?php
+
+if (!defined('BASE_PATH')) {
+    exit('No direct script access allowed');
+}
+
+/**
+ * Kebab Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the  Dual Licensing Model that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.kebab-project.com/licensing
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@lab2023.com so we can send you a copy immediately.
+ *
+ * @category   Kebab (kebab-reloaded)
+ * @package    Modules
+ * @subpackage Kebab
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/licensing
+ * @version    1.5.0
+ */
+
+/**
+ * Kebab Administration Module Turkish Translations
+ *
+ * @category   Kebab (kebab-reloaded)
+ * @package    Modules
+ * @subpackage Kebab
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/licensing
+ * @version    1.5.0
+ */
+return array(
+    'Key' => 'Değer'
+);
