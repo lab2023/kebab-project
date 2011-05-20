fd966e5dde5fbcf9559e31bbbaf596158f9c589e
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-05-20 18:12:38 +0300

    Code review

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,7 +1,5 @@
 <?php
 
-if (!defined('BASE_PATH'))
-    exit('No direct script access allowed');
 /**
  * Kebab Framework
  *
@@ -16,11 +14,11 @@ if (!defined('BASE_PATH'))
  * to info@lab2023.com so we can send you a copy immediately.
  *
  * @category   Kebab (kebab-reloaded)
- * @package    Controllers
- * @subpackage Default
- * @author	   lab2023 Dev Team
+ * @package    Kebab
+ * @subpackage Controllers
+ * @author	   Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
  * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
- * @license    http://www.kebab-project.com/licensing
+ * @license    http://www.kebab-project.com/cms/licensing
  * @version    1.5.0
  */
 
@@ -28,19 +26,15 @@ if (!defined('BASE_PATH'))
  * Kebab Application Index Controller
  *
  * @category   Kebab (kebab-reloaded)
- * @package    Controllers
- * @subpackage Default
- * @author	   lab2023 Dev Team
+ * @package    Kebab
+ * @subpackage Controllers
+ * @author	   Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
  * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
- * @license    http://www.kebab-project.com/licensing
+ * @license    http://www.kebab-project.com/cms/licensing
  * @version    1.5.0
  */
 class IndexController extends Kebab_Controller_Action
 {
-    /**
-     * Home Page
-     * @return void
-     */
     public function indexAction()
     {
 
