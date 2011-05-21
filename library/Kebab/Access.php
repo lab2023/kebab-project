16553c108307b5ac1238a9e89afed0780d137256
Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
2011-05-21 22:33:26 +0300

    Code review

--- library/Kebab/Access.php
+++ library/Kebab/Access.php
@@ -14,9 +14,9 @@
  * to info@lab2023.com so we can send you a copy immediately.
  *
  * @category   Kebab (kebab-reloaded)
- * @package    PACKAGE
- * @subpackage SUB_PACKAGE
- * @author     lab2023 Dev Team
+ * @package    Kebab
+ * @subpackage library
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
  * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
  * @license    http://www.kebab-project.com/cms/licensing
  * @version    1.5.0
