b52397dfdf00de0812db45bfe3d4b682487fc908
onurozgurozkan <onur.ozgur.ozkan@lab2023.com>
2011-07-03 15:34:33 +0300

    Issue #52 add Kebab_List class add Kebab_List_Exception

---
+++ library/Kebab/List/Exception.php
@@ -0,0 +1,41 @@
+<?php
+ 
+/**
+ * Kebab Framework
+ *
+ * LICENSE
+ *
+ * This source file is subject to the  Dual Licensing Model that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.kebab-project.com/cms/licensing
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@lab2023.com so we can send you a copy immediately.
+ *
+ * @category   Kebab (kebab-reloaded)
+ * @package    Library
+ * @subpackage Exception
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/cms/licensing
+ * @version    1.5.0
+ * @date       7/3/11
+ * @time       1:45 PM
+ */
+ 
+/**
+ * 
+ *
+ * @category   Kebab (kebab-reloaded)
+ * @package    Library
+ * @subpackage Exception
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/cms/licensing
+ * @version    1.5.0
+ */
+class Kebab_List_Exception extends Kebab_Exception
+{
+
+}
