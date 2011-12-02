50dd858a8263833bc49d5c9631a96f87e567fb20
onurozgurozkan <onur.ozgur.ozkan@lab2023.com>
2011-12-02 17:01:06 +0200

    Issue #91 get all _translate table names.

---
+++ application/modules/kebab/models/Translate.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Kebab Project
+ *
+ * LICENSE
+ *
+ * This source file is subject to the  Dual Licensing Model that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.kebab-project.com/cms/licensing
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to info@lab2023.com so we can send you a copy immediately.
+ *
+ * @category   Kebab
+ * @package    Kebab
+ * @subpackage Model
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/cms/licensing
+ * @version    1.5.0
+ */
+
+/**
+ * Kebab_Model_Role
+ *
+ * @category   Kebab
+ * @package    Kebab
+ * @subpackage Model
+ * @author     Onur Özgür ÖZKAN <onur.ozgur.ozkan@lab2023.com>
+ * @copyright  Copyright (c) 2010-2011 lab2023 - internet technologies TURKEY Inc. (http://www.lab2023.com)
+ * @license    http://www.kebab-project.com/cms/licensing
+ * @version    1.5.0
+ */
+class Kebab_Model_Translate
+{
+    static public function getTranslationTables()
+    {
+        $statement = Doctrine_Manager::getInstance()->connection();
+        $query = $statement->execute("SHOW TABLES");
+        $translateTables = array();
+        foreach($query->fetchAll() as $table) {
+            if(preg_match("/_translation/", $table[0])) {
+                $translateTables[] = $table[0];
+            }
+        }
+        return $translateTables;
+    }
+}
